d4968dde84dd16682b81d3d6ff8573635186c03d
Ermiq <58442318+Ermiq@users.noreply.github.com>
2021-08-02 15:17:25 +0300

    update Ring.cs

--- Scripts/Terrain/Ring.cs
+++ Scripts/Terrain/Ring.cs
@@ -14,7 +14,18 @@ public class Ring
 			{
 				Chunk chunk = new Chunk(noise, material, new Vector2(j, k), size);
 				chunks.Add(chunk);
+				chunk.SetDetail(detail + (index - 1));
+				chunk.Generate();
+				chunk.Translation = new Vector3(j * size, 0, k * size);
 			}
+		}
+	}
+	
+	public Shift(float offsetX, float offsetY)
+	{
+		foreach (Chunk chunk in chunks)
+		{
+			chunk.Translation += new Vector3(offsetX, 0, offsetY);
 		}
 	}
 }
